87bae6da735c3e187ee5ed3ec2a4eb39ed3342fa
DESKTOP-3OTLM8O\Mata <mperkovic@protonmail.com>
2022-01-29 12:23:24 +0100

    add handlebars

--- src/app.js
+++ src/app.js
@@ -5,8 +5,29 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 const app = express();
 
+app.set('view engine', 'hbs');
+
 app.use(express.static(publicDirectoryPath));
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Matija'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Matija'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'Help!',
+    });
+})
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -17,4 +38,4 @@ app.get('/weather', (req, res) => {
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.');
-})
+});
