2996b12084bdb38538c870596cfe10ac43de2ce5
DESKTOP-3OTLM8O\Mata <mperkovic@protonmail.com>
2022-01-29 12:53:57 +0100

    add partials

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const express = require('express');
-
-const publicDirectoryPath = path.join(__dirname, '../public');
+const hbs = require('hbs');
 
 const app = express();
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirectoryPath));
 
@@ -26,6 +33,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         message: 'Help!',
+        title: 'Help',
+        name: 'Matija'
     });
 })
 
